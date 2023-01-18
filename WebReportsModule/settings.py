34a2ab45bf7a8508ba00d5d4d6f320f1a786c9f1
Mikhail Rozenberg <ro3enn@yandex.ru>
2023-01-18 10:08:31 +0300

    Получилось увеличить скорость составления отчёта, но этого всё равно недостаточно

--- WebReportsModule/settings.py
+++ WebReportsModule/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&+gl#*-h+dt+cnau6q=n292vhcfajgxz9trourvcaoj42isg=)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 LOGIN_REDIRECT_URL = '/'
